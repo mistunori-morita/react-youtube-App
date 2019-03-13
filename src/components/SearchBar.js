ca14060998d3b8ac5fcc050d1b57d4b77aab8f0c
mitsunori.morita <mitsunori.morita@speee.jp>
2019-03-13 16:51:29 +0900

    Add semanthic-ui

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,19 @@
 import React, { Component } from "react";
 
 export default class SearchBar extends Component {
+  state = {
+    term: "test"
+  };
   render() {
-    return <div>SearchBar</div>;
+    return (
+      <div className="search-bar ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Viedo Search</label>
+            <input type="text" value={this.state.term} />
+          </div>
+        </form>
+      </div>
+    );
   }
 }
